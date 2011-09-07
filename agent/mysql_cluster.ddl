2f10faf8ee223a3b96aa93bd8f20f1eb888d606d
Mike Marion <marionm@businesslogic.com>
2011-09-07 17:15:00 -0500

    Removing server ID generation, as that is now done in a different project

--- agent/mysql_cluster.ddl
+++ agent/mysql_cluster.ddl
@@ -2,7 +2,7 @@ metadata(
   :name        => 'MySQL Cluster',
   :description => 'Agent for managing MySQL instances in a cluster',
   :author      => 'Mike Marion',
-  :version     => '0.0.1',
+  :version     => '0.0.2',
   :url         => 'https://www.github.com/BLC/mcollective',
   :license     => '',
   :timeout     => 60
--- agent/mysql_cluster.rb
+++ agent/mysql_cluster.rb
@@ -5,7 +5,7 @@ module MCollective
         :name        => 'MySQL Cluster',
         :description => 'Agent for managing MySQL instances in a cluster',
         :author      => 'Mike Marion',
-        :version     => '0.0.1',
+        :version     => '0.0.2',
         :url         => 'https://www.github.com/BLC/mcollective',
         :license     => '',
         :timeout     => 60
@@ -14,15 +14,12 @@ module MCollective
       action 'promote' do
         validate :yaml_facts, String
 
-        server_id = unique_server_id
-
-        set_server_id(server_id)
         enable_bin_log
         reload_mysql
 
-        set_facts(request[:yaml_facts], server_id, true)
+        set_facts(request[:yaml_facts], true)
 
-        reply.data = "New master server ID: #{server_id}"
+        reply.data = "MySQL instance now able to be a master"
       end
 
       action 'enslave' do
@@ -30,41 +27,13 @@ module MCollective
         validate :repl_user, String
         validate :repl_password, String
 
-        server_id = unique_server_id
-
-        set_server_id(server_id)
+        master = master_hostname
         enslave(request[:repl_user], request[:repl_password], request[:root_password])
         reload_mysql
 
-        set_facts(request[:yaml_facts], server_id, false)
+        set_facts(request[:yaml_facts], false)
 
-        reply.data = "New slave server ID: #{server_id}"
-      end
-
-      #TODO: How to do discovery directly from within an agent?
-      #TODO: Can server ID generation (and therefore this entire thing) be pushed into chef?
-      #      All unique things about the instance seem to form too large of an integer ID, even after
-      #      applying some non-colliding operation, so discovery would have to be done mid Chef run.
-      def unique_server_id
-        status, out, err = run('mco facts mysql_server_id -W "role.mysql_server cluster_member=true"')
-        reply.fail! "Could not query environment: #{err}" unless status == 0
-
-        existing_ids = out.map! { |line| extract_fact(line).to_i }
-        existing_ids.delete(nil)
-        existing_ids.sort
-
-        id = 1
-        while existing_id = existing_ids.shift
-          return id if existing_id != id
-          id += 1
-        end
-        id
-      end
-
-      def set_server_id(server_id)
-        command = 's/^\s*#*\s*\(server-id\)\s*=.*/\1 = ' + server_id.to_s + '/'
-        status, out, err = run(%{sudo sed -i "#{command}" /etc/mysql/my.cnf})
-        reply.fail! "Could not set server id: #{err}" unless status == 0
+        reply.data = "MySQL instance is now a slave of #{master}"
       end
 
       def enable_bin_log
@@ -73,12 +42,12 @@ module MCollective
         reply.fail! "Could not enable bin log: #{err}" unless status == 0
       end
 
-      def enslave(repl_user, repl_password, root_password)
+      def enslave(master, repl_user, repl_password, root_password)
         status, out, err = run("mysql -u root #{"-p#{root_password}" if root_password} -e ''")
         reply.fail! "Could not connect to DB instance: #{err}" unless status == 0
 
         command = "change master to "
-        command << "master_host='#{master_hostname}' "
+        command << "master_host='#{master}' "
         command << "master_user='#{repl_user}' "
         command << "master_password='#{repl_password}' "
         command << "master_log_file='#{}' "
@@ -108,10 +77,10 @@ module MCollective
         line[/\s*(.*)\s*found \d+ times/, 1]
       end
 
-      def set_facts(facts_file, server_id, master)
+      def set_facts(facts_file, master)
         yaml_facts = YAML.load_file(facts_file)
 
-        facts = { :mysql_server_id => server_id, :cluster_member => true, :cluster_master => master }
+        facts = { :cluster_member => true, :cluster_master => master }
         facts.each do |fact, value|
           yaml_facts[fact.to_s] = value
         end
